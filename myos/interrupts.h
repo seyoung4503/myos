9a1c05e6f7847ad74e7f2ac2e14a999d638fc34e
seyoung4503 <seyoung4503@gmail.com>
2024-02-07 16:31:30 +0900

    ADD pic

--- myos/interrupts.h
+++ myos/interrupts.h
@@ -35,7 +35,10 @@ protected:
         uint8_t DescriptorType
     );
 
-
+    Port8BitSlow picMasterCommand;
+    Port8BitSlow picMasterData;
+    Port8BitSlow picSlaveCommand;
+    Port8BitSlow picSlaveData;
 
 
 public:
