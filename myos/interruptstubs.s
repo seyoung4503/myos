ea45b1c4785f4d89cb7fc2425ef06e0ca7e91584
seyoung4503 <seyoung4503@gmail.com>
2024-02-08 10:44:08 +0900

    FIX interruptstubs.s error

--- myos/interruptstubs.s
+++ myos/interruptstubs.s
@@ -5,7 +5,7 @@
 
 .extern  _ZN16InterruptManager15handleInterruptEhj
 
-.macro HandlerException num
+.macro HandleException num
 .global _ZN16InterruptManager16HandleException\num\()Ev
 _ZN16InterruptManager16HandleException\num\()Ev:
     movb $\num, (interruptnumber)
--- myos/kernel.cpp
+++ myos/kernel.cpp
@@ -60,7 +60,7 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*magicnumb
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(&gdt);
 
-    // interrupts.Activate();
+    interrupts.Activate();
 
     while(1);
 }
