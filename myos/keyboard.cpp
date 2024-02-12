15bb5c466ebb5daad9ce0926b83098539f491c66
seyoung4503 <seyoung4503@gmail.com>
2024-02-12 11:17:20 +0900

    FIX keyboard driver

--- myos/keyboard.cpp
+++ myos/keyboard.cpp
@@ -29,10 +29,21 @@ uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp)
 {
     uint8_t key = dataport.Read();
 
-    char* foo = "KEYBOARD 0x00 ";
-    char* hex = "0123456789ABCDEF";
-    foo[11] = hex[(key >> 4) & 0x0F];
-    foo[12] = hex[key & 0x0F];
-    printf(foo);
+    switch (key)
+    {
+    case 0xFA:break;
+    case 0x45:
+    case 0xC5:
+        break;
+    
+    default:
+        char* foo = "KEYBOARD 0x00 ";
+        char* hex = "0123456789ABCDEF";
+        foo[11] = hex[(key >> 4) & 0x0F];
+        foo[12] = hex[key & 0x0F];
+        printf(foo);
+        break;
+    }
+    
     return esp;
 }
